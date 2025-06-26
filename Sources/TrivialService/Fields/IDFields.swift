fa43171c0433f3f1d772336081343e02b4ae8186
Jordan Kay <jkay@collegeboard.org>
2025-06-25 20:22:47 -0400

    Remove decodable conformance from ID fields

--- Sources/TrivialService/Fields/IDFields.swift
+++ Sources/TrivialService/Fields/IDFields.swift
@@ -9,17 +9,3 @@ import struct Identity.Identifier
 extension IDFields: TrivialService.QuestionFields where Model == Question.Identified {}
 extension IDFields: TrivialService.AnswerFields where Model == Answer.Identified {}
 extension IDFields: TrivialService.CategoryFields where Model == Category.Identified {}
-
-extension IDFields: Swift.Decodable where Model.ID: Decodable {
-	public init(from decoder: any Decoder) throws {
-		let container = try decoder.container(keyedBy: CodingKeys.self)
-		try self.init(id: container.decode(Model.ID.self, forKey: .id))
-	}
-}
-
-// MARK: -
-private extension IDFields {
-	enum CodingKeys: String, CodingKey {
-		case id
-	}
-}
